fdc3bd27b6f3305473bfc80a8273832ec156200d
Elmore Vaal <vaal@ph2.uni-koeln.de>
2023-11-17 11:25:01 +0100

    Bugfix: allow doubleclicking of files/folders with spaces

--- src/ListBox.cpp
+++ src/ListBox.cpp
@@ -1,8 +1,10 @@
 #include "ListBox.h"
-//#include "MyStrings.h"
+#include "MyStrings.h"
 #include "shellapi.h"
 
 /*::ShellExecute uses Shell32.dll*/
+
+// TODO: ShellExecute cannot handle 8.3-style filepaths when the filepath is above 259/260 (with NULL) characters
 
 bool Win::ListBoxController::OnLButtonDoubleClick(int x, int y, KeyState kState)
 {
@@ -13,10 +15,13 @@ bool Win::ListBoxController::OnLButtonDoubleClick(int x, int y, KeyState kState)
 
 		if (wcscmp(str.c_str(),L"No search results")!=0 && str.length()>0)
 		{
-			std::wstring selectPhrase=L"/select,";
+			std::wstring selectPhrase=L"/select,\""; 
 			selectPhrase+=str.c_str();
-			::ShellExecute(list,L"open",L"explorer.exe",selectPhrase.c_str(),0,SW_SHOWNORMAL);
-			return 0;
+			selectPhrase+=L"\""; // Have file path in quotes to handle spaces in file path
+			HINSTANCE res = ::ShellExecute(list,L"open",L"explorer.exe",selectPhrase.c_str(),0,SW_SHOWNORMAL);
+			//res returns >32 if successful
+			if (res>(HINSTANCE)32)
+			{return 0;}
 		}
 	};
 	return 1;
