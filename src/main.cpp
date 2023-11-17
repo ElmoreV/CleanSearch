dcff92e7b9ee9f4917a127125bd306757f6a1d55
Elmore Vaal <vaal@ph2.uni-koeln.de>
2023-11-17 11:31:54 +0100

    Update version number

--- src/main.cpp
+++ src/main.cpp
@@ -6,20 +6,19 @@
 #include"MyController.h"
 #include"Resource.h"
 
-
-
-wchar_t version[255]=L"v1.0";
+wchar_t version[255]=L"v1.1";
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, char* cmdLine, int cmdShow)
 {
 	try
 	{
+		// Create a Window, add the EventHandler (Controller), and start waiting/handling messages.
 		Win::Class winclass(hInstance,L"mainclass",Win::WndProc);
 		winclass.SetResIcons(IDI_ICON1);
 		winclass.Register();
 		Win::Creator winMaker(winclass);
 		TopController Ctrl;
-		wchar_t title[255]=L"Finally finished, enjoy CleanSearch ";
+		wchar_t title[255]=L"Fresh as ever, enjoy Cleansearch ";
 		wcscat_s(title,255,version);
 		Win::Dow win(winMaker.Create(Ctrl,title));
 		win.Show(cmdShow);
